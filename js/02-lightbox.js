d461f1d1c375b569811b917ad458baa743750d5d
Oleg Subotin <subotinoleg0@gmail.com>
2022-02-14 23:27:59 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createImagesMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryRef.addEventListener('click', onImgClick);
+
+function createImagesMarkup(img) {
+    return img.map(item =>
+        `<a class="gallery__item" 
+        href="${item.original}">
+        <img class="gallery__image" 
+        src="${item.preview}" 
+        alt="${item.description}" />
+        </a>`
+    ).join('');
+}
+
+function onImgClick(event) {
+    event.preventDefault();
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt",  captionDelay: 250});
+}
